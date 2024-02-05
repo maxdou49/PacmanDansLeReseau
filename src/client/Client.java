b0c8738217286b3cade9eec04f1eee85be30f118
yyouss <yyouss@etud.univ-angers.fr>
2024-02-05 16:14:12 +0100

    maj

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,11 +1,36 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import controller.ControleurPacmanGame;
 
 public class Client {
-    public static void main(String[] args)
+    public static void main(String[] argu)
     {
+        //ControleurPacmanGame game = new ControleurPacmanGame("layout/originalClassic.lay");
+        //game.play();
 
-        
-        ControleurPacmanGame game = new ControleurPacmanGame("layout/originalClassic.lay");
-        game.play();
+        int p=1234; // le port d’écoute
+        Socket so;
+
+        BufferedReader entree;
+        PrintWriter sortie;
+
+        try {
+            so = new Socket("localhost", p);
+
+            entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+            sortie = new PrintWriter (so.getOutputStream(), true); 
+
+            String message = entree.readLine();
+
+            if(message.equals("Lance"))
+                System.out.println("Lancement du jeu");
+                
+        } catch (IOException e) { System.out.println("problème\n"+e); }
     }
 }
--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,9 +1,36 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import controller.ControleurPacmanGame;
 
 public class Serveur {
     public static void main(String[] args)
     {
-        ControleurPacmanGame game = new ControleurPacmanGame("layout/originalClassic.lay");
-        game.play();
+        //ControleurPacmanGame game = new ControleurPacmanGame("layout/originalClassic.lay");
+        //game.play();
+
+        int p=1234; // le port d’écoute
+        ServerSocket ecoute;
+        Socket so;
+        BufferedReader entree;
+        PrintWriter sortie;
+        
+        try {
+            ecoute = new ServerSocket(p); // on crée le serveur
+            System.out.println("serveur mis en place ");
+            while(true) {// le serveur va attendre qu’une connexion arrive
+                so = ecoute.accept();
+                System.out.println("Client connecté");
+                entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+                sortie = new PrintWriter (so.getOutputStream(), true);    
+                sortie.println("Lance");
+            }
+        } catch (IOException e) { System.out.println("problème\n"+e); }
+    
     }
 }
