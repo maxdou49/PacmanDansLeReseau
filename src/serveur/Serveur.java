ab49ae5c26b4d6299b7deb9e97f723d42a7f4952
yyouss <yyouss@etud.univ-angers.fr>
2024-02-08 10:06:57 +0100

    maj

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Serveur {
     public static void main(String[] args)
